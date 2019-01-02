37918684b5ea1ad2497166bf95bcb0306d831ce3
Henrik Steen <henrist@henrist.net>
2019-01-02 05:36:04 +0100

    Release 0.2.0

--- sceptre_s3_packager/__init__.py
+++ sceptre_s3_packager/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from sceptre_s3_packager.s3_packager import KeyResolver, UploadHook  # noqa: F401, E501
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Henrik Steen'
 
 
